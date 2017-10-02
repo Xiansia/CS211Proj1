e57e59c057b4b5776b2a123f54220931da9d1d7f
Conway Dang <conwaydang@conways-macbook-air.local>
2017-10-02 10:11:29 -0500

    Initial Commit

---
+++ cs211proj2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs211proj2
+//
+//  Created by Conway Dang on 10/2/17.
+//  Copyright © 2017 Conway Dang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
